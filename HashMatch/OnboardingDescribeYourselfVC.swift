d8cec8ea9c2707fae89390c87c5b80b86c07f420
Jaeyoung Martin <jymartin@wustl.edu>
2020-11-26 18:23:23 -0600

    Pic ur profile pic

--- HashMatch/OnboardingDescribeYourselfVC.swift
+++ HashMatch/OnboardingDescribeYourselfVC.swift
@@ -12,10 +12,71 @@ class OnboardingDescribeYourselfVC: UIViewController {
 
     @IBOutlet weak var nextbtn: UIButton!
     @IBOutlet weak var describeYourselfTextView: UITextView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         describeYourselfTextView.textViewDesign()
         nextbtn.applyPrimaryBtnDesign()
-        // Do any additional setup after loading the view.
+        
+        //setup initial imageView, idk how to do it in storyboard
+        imageView.layer.cornerRadius = imageView.frame.size.width/2
+        imageView.image = UIImage(systemName: "person")
+        imageView.tintColor = .gray
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 2
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        imageView.isUserInteractionEnabled = true
+        
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(changeProfilePic))
+        gesture.numberOfTouchesRequired = 1
+        imageView.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func changeProfilePic() {
+        presentPhotoActionSheet()
+    }
+}
+
+extension OnboardingDescribeYourselfVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func presentPhotoActionSheet() {
+        let actionSheet = UIAlertController(title: "Profile Picture", message: "How would you like to select a picture?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: {[weak self]_ in self?.presentCamera()}))
+        actionSheet.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: {[weak self]_ in self?.presentPhotoPicker()}))
+        present(actionSheet, animated: true)
+    }
+    
+    func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        self.imageView.image = image
     }
 }
