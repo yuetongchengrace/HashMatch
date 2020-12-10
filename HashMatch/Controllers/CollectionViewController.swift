e4af2434710d993d33e80ad1fb82cf6e66e21c83
ashley <ashleylin1@wustl.edu>
2020-12-09 18:43:42 -0800

    changed front end of the colllectionView on the "Explore" page- Ashley

--- HashMatch/Controllers/CollectionViewController.swift
+++ HashMatch/Controllers/CollectionViewController.swift
@@ -1,10 +1,7 @@
-//
 //  CollectionViewController.swift
 //  HashMatch
-//
 //  Created by Yuetong Chen on 11/23/20.
 //  Copyright © 2020 Yuetong Chen. All rights reserved.
-//
 
 import UIKit
 import Firebase
@@ -17,10 +14,14 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(true, animated: false)
-        // Do any additional setup after loading the view.
         if let id = UserDefaults.standard.string(forKey: "user"){
             userId = id
         }
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 2
+        layout.minimumLineSpacing = 12
+        collectionView!.collectionViewLayout = layout
+        
         fetchData()
         setUpCollectionView()
     }
@@ -77,63 +78,107 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     }
     func cacheImages(){
        for (index, person) in people.enumerated() {
-        if let fullURL =  URL(string: person.photo){
-           // print(fullURL!)
-           do{
-               let data = try Data(contentsOf: fullURL)
-               let img = UIImage(data:data)
-               // print(index)
-               images[index] = img!
-               
-           }
-           catch {
-               print("There was an error")
-           }
-       }
+            if let fullURL =  URL(string: person.photo){
+               // print(fullURL!)
+               do{
+                   let data = try Data(contentsOf: fullURL)
+                   let img = UIImage(data:data)
+                   // print(index)
+                   images[index] = img!
+                   
+               }
+               catch {
+                   print("There was an error")
+               }
+            }
         }
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.people.count;
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PersonCell
-        cell.backgroundColor = UIColor.gray
-        cell.pic.image = images[indexPath.row]
-        cell.name.text = people[indexPath.row].firstName + " " + people[indexPath.row].lastName
+        cell.name.text = "  " + people[indexPath.row].firstName + " " + people[indexPath.row].lastName
         cell.addSubview(cell.pic)
-        cell.addSubview(cell.name)
-        return cell as PersonCell
+        //cell.addSubview(cell.name)
         
+        //cell styling
+        cell.backgroundColor = UIColor.gray
+        cell.layer.cornerRadius = 20
+        cell.heightAnchor.constraint(equalToConstant: 210 ).isActive = true
+        cell.widthAnchor.constraint(equalToConstant: 166 ).isActive = true
+        cell.layer.borderWidth = 0.5
+        cell.layer.borderColor = UIColor.black.cgColor
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOffset = CGSize(width: 1, height: 1)
+        cell.layer.shadowRadius = 2.0
+        cell.layer.shadowOpacity = 0.5
+        cell.layer.masksToBounds = false
+        
+        //make cell image centered
+        let cellPic = cell.pic
+        cellPic.translatesAutoresizingMaskIntoConstraints = false
+        cellPic.leftAnchor.constraint(equalTo: cell.leftAnchor).isActive = true
+        cellPic.rightAnchor.constraint(equalTo: cell.rightAnchor).isActive = true
+        cellPic.topAnchor.constraint(equalTo: cell.topAnchor).isActive = true
+        cellPic.bottomAnchor.constraint(equalTo: cell.bottomAnchor).isActive = true
+        
+        //cell image styling
+        cellPic.image = images[indexPath.row]
+        cellPic.contentMode = .scaleAspectFill
+        cellPic.layer.cornerRadius = 20
+        cellPic.clipsToBounds = true
+        
+        //cell text styling
+        let cgrect1 = CGRect.init(x: 10, y: 165, width: 146, height: 28)
+        let nameLabel = UILabel.init(frame: cgrect1)
+        nameLabel.text = people[indexPath.row].firstName + " " + people[indexPath.row].lastName
+        cell.addSubview(nameLabel)
+        nameLabel.layer.cornerRadius = 14
+        nameLabel.layer.backgroundColor = UIColor.white.cgColor
+        nameLabel.textColor = UIColor.hashOrange
+        //nameLabel.font = nameLabel.font.withSize(10)
+        nameLabel.font = UIFont(name: "Gill Sans", size: 15)
+        nameLabel.font = UIFont.boldSystemFont(ofSize: nameLabel.font.pointSize)
+        nameLabel.textAlignment = NSTextAlignment.center
+       
+        return cell as PersonCell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("touched")
 //        let detailedVC = DetailedViewController()
 //        detailedVC
-//
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let secondViewController  = storyboard.instantiateViewController(withIdentifier: "Detailpage") as? DetailedViewController else { return }
         secondViewController.person = people[indexPath.row]
         // self.present(secondViewController, animated: true, completion: nil)
         self.navigationController?.pushViewController(secondViewController, animated: true)
-        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension CollectionViewController : UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-       return CGSize(width: 150, height: 200)
+       return CGSize(width: 166, height: 210)
     }
 }
+extension UIColor{
+    static let hashOrange = UIColor(red: 255.0/255, green: 67.0/255, blue: 60.0/255, alpha: 1.0)
+}
+
+//old cell text styling
+//        //cell text styling
+//        let cellText = cell.name
+//        cellText.centerXAnchor.constraint(equalTo: cell.centerXAnchor).isActive = true
+//        cellText.textAlignment = NSTextAlignment.center
+//        cellText.layer.borderWidth = 0.5
+//        cellText.layer.borderColor = UIColor.black.cgColor
+//        cellText.layer.backgroundColor = UIColor.white.cgColor
+//        cellText.textColor = UIColor.black
+
+//        //make cell text centered
+//        cellText.translatesAutoresizingMaskIntoConstraints = false
+//        cellText.heightAnchor.constraint(equalToConstant: 22).isActive = true
+//        cellText.widthAnchor.constraint(equalToConstant: 130).isActive = true
+//        cellText.leftAnchor.constraint(equalTo: cell.leftAnchor).isActive = true
+//        cellText.rightAnchor.constraint(equalTo: cell.rightAnchor).isActive = true
